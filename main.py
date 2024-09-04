f33a85038d757678506aaa4e6e95971c0228f57b
Dmitry Krivoruchko <krivoruchko.di@phystech.edu>
2024-09-04 20:37:58 +0300

    feat(data_analysis): first try to parse (fail because of API existence)

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import requests
-import bs4
+from bs4 import BeautifulSoup
 import lxml
+
+url_all = "https://www.ebi.ac.uk/chembl/g/#search_results/all"
+
+# ответ с сайта
+response = requests.get(url_all)
+
+soup = BeautifulSoup(response.text, "lxml")
+print(soup)
+
+# ошибка из-за того, что элементы подгружаются динамически
+# data = soup.find("div", class_="col s12 m6 l4")
+
+# name = data.find("p", class_="p-oneline")
+# print(name)
