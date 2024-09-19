eb641ebc534ef040e2954d43e4afd433649faa80
Dmitry Krivoruchko <krivoruchko.di@phystech.edu>
2024-09-19 21:44:12 +0300

    chore(data_analysis): update ChEMBL_download (pre-finish)

--- ChEMBL_download/download.py
+++ ChEMBL_download/download.py
@@ -1,1 +1,76 @@
-import chembl_webresource_client
+# from icecream import ic
+from os import mkdir
+from functions import *
+from loguru import logger
+import sys
+
+
+# Loguru settings
+logger.remove()
+logger.add(sink=sys.stderr,
+           format="<level>{level}</level>: <yellow>ChEMBL_download:</yellow> <white>{message}</white>")
+# ic.disable()
+
+
+def DownloadMWRange(less_limit: int = 0, greater_limit: int = 12_546_42):
+    """
+    DownloadMolecularWeightRange - функция, которая скачивает молекулы в .csv файл с выводом информации
+    из базы ChEMBL по диапазону молекулярного веса (обе границы включительно)
+
+    Args:
+        less_limit (int, optional): нижняя граница. Defaults to 0.
+        greater_limit (int, optional): верхняя граница. Defaults to 12_546_42.
+    """
+
+    try:
+        logger.info(
+            f"Downloading molecules with molecular weight in range [{less_limit}, {greater_limit}]...")
+        mw_mols_in_range: QuerySet = QuerySetMWRangeFilter(
+            less_limit, greater_limit)
+        logger.info(f"Amount: {len(mw_mols_in_range)}")  # type: ignore
+        logger.success(
+            f"Downloading molecules with molecular weight in range [{less_limit}, {greater_limit}]: SUCCESS")
+
+        try:
+            logger.info("Collecting molecules to pandas.DataFrame()...")
+            data = pd.DataFrame(mw_mols_in_range)  # type: ignore
+            logger.success("Collecting molecules pandas.DataFrame(): SUCCESS")
+
+            logger.info(
+                "Collecting molecules from pandas.DataFrame to .csv file in results...")
+            data = ExpandedFLDF(data)
+
+            file_name: str = f"results/range_{
+                less_limit}_{greater_limit}_mw_mols.csv"
+
+            data.to_csv(file_name, index=False)
+            logger.success(
+                "Collecting molecules from pandas.DataFrame to .csv file in results: SUCCESS")
+
+        except Exception as exception:
+            logger.error(exception)
+
+    except Exception as exception:
+        logger.error(exception)
+
+
+def Download_ChEMBL():
+    try:
+        logger.info("Creating folder 'results'...")
+        mkdir("results")
+        logger.success("Creating folder 'results': SUCCESS")
+
+    except Exception as exception:
+        logger.error(exception)
+
+    mw_ranges: list[tuple[int, int]] = [(0, 100), (100, 200), (200, 300),
+                                        (300, 400), (400, 500), (500, 600),
+                                        (600, 700), (700, 800), (800, 900),
+                                        (900, 1000), (1000, 12_546_42)]
+
+    for less_limit, greater_limit in mw_ranges:
+        DownloadMWRange(less_limit, greater_limit)
+
+
+if __name__ == "__main__":
+    Download_ChEMBL()
---
+++ ChEMBL_download/functions.py
@@ -0,0 +1,94 @@
+from chembl_webresource_client.new_client import new_client
+from chembl_webresource_client.query_set import QuerySet
+import pandas as pd
+
+
+def QuerySetMWRangeFilter(less_limit: int = 0, greater_limit: int = 12_546_42) -> QuerySet:
+    """      
+    QuerySetMolecularWeightRangeFilter - функция, которая выполняет фильтрацию по базе ChEMBL
+    по диапазону молекулярного веса (обе границы включительно)
+
+    Args:
+        less_limit (int, optional): нижняя граница. Defaults to 0
+        greater_limit (int, optional): верхняя граница. Defaults to 12_546_42
+
+    Raises:
+        ValueError: если границы меньше являются отриц. числами
+        ValueError: если, верхняя граница больше нижней
+
+    Returns:
+        QuerySet: список объектов заданной модели
+    """
+
+    if greater_limit < 0 or less_limit < 0:
+        raise ValueError(
+            "QuerySetMWRangeFilter: limits should be greater zero")
+
+    if greater_limit < less_limit:
+        raise ValueError(
+            "QuerySetMWRangeFilter: greater_limit should be greater than less_limit")
+
+    return new_client.molecule.filter(molecule_properties__mw_freebase__lte=greater_limit,  # type: ignore
+                                      molecule_properties__mw_freebase__gte=less_limit)
+
+
+def ExpandedFLDF(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    ExpandedFromListsDataFrame - функция, которая переписывает словари и списки словарей 
+    в таблице в отдельные столбцы
+
+    Args:
+        data (pd.DataFrame): изначальная таблица
+
+    Returns:
+        pd.DataFrame: "раскрытая" таблица
+    """
+
+    exposed_data = pd.DataFrame({
+        #! cross_references
+        'xref_id':                     data['cross_references'].apply(lambda x: [d['xref_id'] for d in x] if x else []),
+        'xref_name':                   data['cross_references'].apply(lambda x: [d['xref_name'] for d in x] if x else []),
+        'xref_src':                    data['cross_references'].apply(lambda x: [d['xref_src'] for d in x] if x else []),
+        #! molecule_hierarchy
+        'active_chembl_id':            [item['active_chembl_id'] if isinstance(item, dict) else None for item in data['molecule_hierarchy']],
+        'molecule_chembl_id':          [item['molecule_chembl_id'] if isinstance(item, dict) else None for item in data['molecule_hierarchy']],
+        'parent_chembl_id':            [item['parent_chembl_id'] if isinstance(item, dict) else None for item in data['molecule_hierarchy']],
+        #! molecule_properties
+        'alogp':                       [item['alogp'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'aromatic_rings':              [item['aromatic_rings'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'cx_logd':                     [item['cx_logd'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'cx_logp':                     [item['cx_logp'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'cx_most_apka':                [item['cx_most_apka'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'cx_most_bpka':                [item['cx_most_bpka'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'full_molformula':             [item['full_molformula'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'full_mwt':                    [item['full_mwt'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'hba':                         [item['hba'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'hba_lipinski':                [item['hba_lipinski'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'hbd':                         [item['hbd'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'hbd_lipinski':                [item['hbd_lipinski'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'heavy_atoms':                 [item['heavy_atoms'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'molecular_species':           [item['molecular_species'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'mw_freebase':                 [item['mw_freebase'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'mw_monoisotopic':             [item['mw_monoisotopic'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'np_likeness_score':           [item['np_likeness_score'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'num_lipinski_ro5_violations': [item['num_lipinski_ro5_violations'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'num_ro5_violations':          [item['num_ro5_violations'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'psa':                         [item['psa'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'qed_weighted':                [item['qed_weighted'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'ro3_pass':                    [item['ro3_pass'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        'rtb':                         [item['rtb'] if isinstance(item, dict) else None for item in data['molecule_properties']],
+        #! molecule_structures
+        'canonical_smiles':            [item['canonical_smiles'] if isinstance(item, dict) else None for item in data['molecule_structures']],
+        # 'molfile':                   [item['molfile'] if isinstance(item, dict) else None for item in data['molecule_structures']], - какая-то стрёмная хрень с RDKit
+        'standard_inchi':              [item['standard_inchi'] if isinstance(item, dict) else None for item in data['molecule_structures']],
+        'standard_inchi_key':          [item['standard_inchi_key'] if isinstance(item, dict) else None for item in data['molecule_structures']],
+        #! molecule_synonyms
+        'molecule_synonym':            data['molecule_synonyms'].apply(lambda x: [d['molecule_synonym'] for d in x] if x else []),
+        'syn_type':                    data['molecule_synonyms'].apply(lambda x: [d['syn_type'] for d in x] if x else []),
+        'synonyms':                    data['molecule_synonyms'].apply(lambda x: [d['synonyms'] for d in x] if x else []),
+    })
+
+    data = data.drop(['cross_references', 'molecule_hierarchy',
+                     'molecule_properties', 'molecule_structures', 'molecule_synonyms'], axis=1)
+
+    return pd.concat([data, exposed_data], axis=1)
