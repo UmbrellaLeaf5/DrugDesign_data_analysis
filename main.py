4502289401cd164010b015a89f21b2f10e7eabde
Dmitry Krivoruchko <krivoruchko.di@phystech.edu>
2024-10-10 13:25:21 +0300

    fix(data_analysis): fix logical mistakes in compounds_download

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# from ChEMBL_download_compounds.download import DownloadChEMBLCompounds
+from ChEMBL_download_compounds.download import DownloadChEMBLCompounds
 from ChEMBL_download_targets.download import DownloadChEMBLTargets
 
 # from PubChem_download.download import
 
 if __name__ == "__main__":
+    DownloadChEMBLCompounds(skip_downloaded_files=True)
     DownloadChEMBLTargets(need_primary_analysis=True)
