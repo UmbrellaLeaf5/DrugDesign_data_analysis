70f0f02d5d5eb9291d6356808af2a9c9cf877ef3
Dmitry Krivoruchko <krivoruchko.di@phystech.edu>
2024-10-17 19:09:27 +0300

    chore(data_analysis): update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from ChEMBL_download_targets.download import DownloadChEMBLTargets
 # from PubChem_download.download import
 
 if __name__ == "__main__":
-    DownloadChEMBLCompounds(skip_downloaded_files=True)
-    DownloadChEMBLTargets(need_primary_analysis=True)
+    DownloadChEMBLTargets()
+    # DownloadChEMBLCompounds(skip_downloaded_files=True)
